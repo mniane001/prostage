f865cbef3e5f8414f11151d798ee92e5e7213144
Mouhamadou Niane <mniane001@iutbayonne.univ-pau.fr>
2021-12-16 10:17:51 +0100

    first commit with vendors

---
+++ src/Controller/ProstageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProstageController extends AbstractController
+{
+    /**
+     * @Route("/prostage", name="prostage")
+     */
+    public function index(): Response
+    {
+        return $this->render('prostage/index.html.twig', [
+            'controller_name' => 'ProstageController',
+        ]);
+    }
+}
