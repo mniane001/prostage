51532b213277687c7460e06307e43f2332807ac8
Mouhamadou Niane <mniane001@iutbayonne.univ-pau.fr>
2022-01-13 12:35:08 +0100

    Création des différentes pages

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -17,4 +17,32 @@ class ProstageController extends AbstractController
             'controller_name' => 'ProstageController',
         ]);
     }
+    /**
+     * @Route("/entreprises", name="pro_stages-entreprises")
+     */
+    public function entreprises(): Response
+    {
+        return $this->render('pro_stages/entreprises.html.twig', [
+            'controller_name' => 'ProstageController',
+        ]);
+    }
+    
+        /**
+     * @Route("/formations", name="pro_stages-formations")
+     */
+    public function formations(): Response
+    {
+        return $this->render('pro_stages/formations.html.twig', [
+            'controller_name' => 'ProstageController',
+        ]);
+    }
+    
+        /**
+     * @Route("/stages/{id}", name="pro_stages-stages")
+     */
+    public function stages($id): Response
+    {
+        return $this->render('pro_stages/stages.html.twig',['idRessource '=>$id ]);
+
+    }
 }
